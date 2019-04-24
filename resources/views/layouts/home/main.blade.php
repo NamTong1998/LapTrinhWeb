a5a87e5725b49d4d3c1397c0105c892e7cd5fefe
TangHuyenTrangit <tanghuyentrangit1998@gmail.com>
2019-04-24 08:28:29 +0700

    master

--- resources/views/layouts/home/main.blade.php
+++ resources/views/layouts/home/main.blade.php
@@ -29,7 +29,7 @@
 				
 				<a href="category.html">{{ $article1->category->name }}</a>
 			</div>
-			<h3 class="post-title"><a href="{{route('')}}">{{$article1->summary}}</a></h3>
+			<h3 class="post-title"><a href="#">{{$article1->summary}}</a></h3>
 			<ul class="post-meta">
 				<li><a href="author.html">{{$article1->user->user_name}}</a></li>
 				<li>{{$article1->created_at}}</li>
