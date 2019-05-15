088eb378c1a3856458378db613a7c0d644f95ec5
NamTong1998 <nam.th.vnist@gmail.com>
2019-05-15 07:08:09 +0700

    well, final?

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class AdminMiddleware
 {
@@ -20,4 +21,5 @@ class AdminMiddleware
             return $next($request);
         }
         return redirect('/');
+    }
 }
